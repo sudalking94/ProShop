b701feded79c92c27239207668743249537b9bf6
Junwo Jeon <loust3598@naver.com>
2021-07-14 00:49:31 +0900

    Home Screen products listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
+import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <div>
       <Header />
-      <main>
-        <h1>Welcome</h1>
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
       </main>
       <Footer />
     </div>
