3d03cb27ecde2af170ec3cc38e3d527dfbf77218
Junwo Jeon <loust3598@naver.com>
2021-07-20 13:51:58 +0900

    Frontend User Authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 function App() {
   return (
@@ -13,9 +16,11 @@ function App() {
       <main className="py-3">
         <Container>
           <Route path="/" component={HomeScreen} exact />
+          <Route path="/login" component={LoginScreen} />
+          <Route path="/register" component={RegisterScreen} />
+          <Route path="/profile" component={ProfileScreen} />
           <Route path="/product/:id" component={ProductScreen} />
-          <Route path="/cart" component={CartScreen} exact />
-          <Route path="/cart/:id" component={CartScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
       <Footer />
